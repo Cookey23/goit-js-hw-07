13cffd18098764ffd76484dd4cd0fb5d5dd2aa22
Serhii Alavatskyi <alavatsky@gmail.com>
2022-02-15 15:42:30 +0200

    task 2 complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const itemsMarkup = renderGalleryItems(galleryItems);
 
+
+
+gallery.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function renderGalleryItems(galleryItems) {
+    return galleryItems.map(({ original, preview, description }) => 
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    ).join('');
+
+};
+
+const galleryModalShow = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+    captionDelay: 250,
+
+});
 console.log(galleryItems);
